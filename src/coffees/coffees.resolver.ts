dace3a011719922bd7eea32b9cc75b99b6d32cfc
Rudrava Mukherjee <rudravamukherjee.2001@gmail.com>
2024-01-28 13:17:25 +0530

    feat add resolver for :id

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,10 +1,18 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, ID, Query, Resolver } from '@nestjs/graphql';
 import { Coffee } from './entities/coffee.entitiy.ts/coffee.entitiy.ts';
+import { ParseIntPipe } from '@nestjs/common';
 
 @Resolver()
 export class CoffeesResolver {
   @Query(() => [Coffee], { name: 'coffees' })
   async findAll() {
     return [];
+  }
+  @Query(() => Coffee, { name: 'coffee', nullable: true })
+  async findOne(
+    @Args('id', { type: () => ID, name: 'id' }, ParseIntPipe) id: number,
+  ) {
+    console.log(id);
+    return null;
   }
 }
--- src/coffees/entities/coffee.entitiy.ts/coffee.entitiy.ts.ts
+++ src/coffees/entities/coffee.entitiy.ts/coffee.entitiy.ts.ts
@@ -1,8 +1,8 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class Coffee {
-  @Field()
+  @Field(() => ID)
   id: number;
   @Field()
   name: string;
